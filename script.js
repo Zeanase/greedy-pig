40104bd186cabe7c8a5786c5708ea2905c75e3c5
Zeanase <94770496+Zeanase@users.noreply.github.com>
2021-12-12 16:30:05 +1300

    save

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 const buttonNew = document.querySelector('.btn--new');
+const holdButton = document.querySelector('.btn--hold');
+const rollDice = documen.querySelector('btn--roll');
+const player1 = document.querySelector('.player--0');
+const player2 = document.querySelector('.player--1');
 
 const player1Score = document.querySelector('#player--0');
 const player2Score = document.querySelector('#player--1');
@@ -20,10 +24,37 @@ const startGame = function() {
     player2Score.textContent = 0;
     player1Current.textContent = 0;
     player2Current.textContent = 0; 
+
+    player1.classList.add('player--active');
+    player2.classList.remove('player--active');
+    player1.classList.remove('player--winner');
+    player2.classList.remove('player--winner');
+};
+const startGame = () => {
+
 };
 startGame();
 
+const switchPlayer = () => {
+
+};
+
+const rollTheDice = () => {
+  if (isGameRunning === true) {
+  const dice = math.trinc(Math.random() * 6)
+  }
+};
+
+const holdMyScore = () => {
+
+};
+
+rollDice.addEventListener('click', function() {
+  rollTheDice();
+});
+holdButton.addEventListener('click',function() {
+  holdMyScore();
+});
 buttonNew.addEventListener('click', function() {
   startGame(); 
 });
-
